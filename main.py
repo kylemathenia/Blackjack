6c44ddcc34599235753048bda8545f35daf813a5
Jm56450 <106172587+Jm56450@users.noreply.github.com>
2022-05-24 22:38:47 -0500

    fixed Black Jack dealer vs black jack

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from player import Player
 from options import StrategyOptions
 
 # Create players
-jake = Player('Jake',StrategyOptions.BASIC,money=100,standard_bet=50,play_as=False)
-kyle = Player('Kyle',StrategyOptions.BASIC,money=100,standard_bet=50,play_as=False)
+jake = Player('Jake',StrategyOptions.BASIC,money=900,standard_bet=25,play_as=False)
+kyle = Player('Kyle',StrategyOptions.BASIC,money=100,standard_bet=5,play_as=False)
 
 # Create table
-t = Table([jake,kyle],num_decks=1,shoe_shuffle_depth=0,min_bet=5,max_bet=10000,
+t = Table([jake, kyle],num_decks=1,shoe_shuffle_depth=0,min_bet=5,max_bet=10000,
                  blackjack_multiple=1.5,hit_soft_17=True,double_after_split=True,autoplay=False)
 
 t.play()
